3b80b2088d13726901a7e97a099fcce18de27baa
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-30 11:47:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-//your JS code here. If required.
+function getTime(){
+    const time = new Date();
+    let dayString = `${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}`;
 
+    let hr = time.getHours();
+    let timeString = "" ;
 
-function currentTime() {
-  let date = new Date(); 
-  let hh = date.getHours();
-  let mm = date.getMinutes();
-  let ss = date.getSeconds();
-  let session = "AM";
+    //1:20AM ,11:59AM, 12:00PM, 13:45 - 12 =  1:45, 12:35PM
+    if(hr >= 12){
+        if(hr === 12){
+            timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} PM`
+        }
+        else {
+            hr -= 12 ;
+            timeString = `${hr}:${time.getMinutes()}:${time.getSeconds()} PM`
+        }
+    }
+    else {
+        // : AM
+        // : 12 hr clock no modifications needed
+        timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} AM`
+    }
 
-    
-  if(hh > 12){
-      session = "PM";
-   }
-
-   hh = (hh < 10) ? "0" + hh : hh;
-   mm = (mm < 10) ? "0" + mm : mm;
-   ss = (ss < 10) ? "0" + ss : ss;
-    
-   let time = hh + ":" + mm + ":" + ss + " " + session;
-
-  document.getElementById("timer").innerText = time; 
-  let t = setTimeout(function(){ currentTime() }, 1000); 
+    return dayString+", "+timeString;
 
 }
+const p = document.getElementById("timer");
 
-currentTime();
+function timer(){
+    p.innerText = getTime();
+}
+
+setInterval(timer, 1000);
